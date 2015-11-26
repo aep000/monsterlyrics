7e34ab6ec7999456a71d3bfce9cbe74d61786faa
Nik Ammerlaan <nik@monsterlyrics.co>
2015-11-26 09:54:35 -0500

    Trying other things with the graph

--- app.py
+++ app.py
@@ -111,7 +111,6 @@ def index():
                 strokeColor: "rgba(255,255,255,1)",
                 highlightFill: "rgba(255,255,255,1)",
                 highlightStroke: "rgba(255,255,255,1)",
-                data: '''+votes+'''
                 }
                 ]
             }
@@ -162,12 +161,7 @@ def dash():
     except:
         return redirect("/", code=302)
     html ='''
-    <!DOCTYPE HTML>
-    <!--
-    	Spectral by HTML5 UP
-    	html5up.net | @n33co
-    	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-    -->
+
     <html>
     	<head>
     		<title>MonsterLyrics</title>
