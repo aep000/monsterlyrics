933cc2e7a5685702c1f8edcdc0df7fdef5180970
Nik Ammerlaan <nik@monsterlyrics.co>
2015-11-25 23:08:03 -0500

    Removed the menu on the dashboard, changed some values in the search

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ def dash():
     -->
     <html>
     	<head>
-    		<title>Monsterlyrics</title>
+    		<title>MonsterLyrics</title>
     		<meta charset="utf-8" />
     		<meta name="viewport" content="width=device-width, initial-scale=1" />
                 <script src="aep000.neocities.org/assets/js/jquery.min.js"></script>
@@ -193,22 +193,6 @@ def dash():
     				<!-- Header -->
     					<header id="header">
     						<h1><a href="/">Monsterlyrics</a></h1>
-    						<nav id="nav">
-    							<ul>
-    								<li class="special">
-    									<a href="#menu" class="menuToggle"><span>Menu</span></a>
-    									<div id="menu">
-    										<ul>
-    											<li><a href="index.html">Home</a></li>
-    											<li><a href="generic.html">Generic</a></li>
-    											<li><a href="elements.html">Elements</a></li>
-    											<li><a href="#">Sign Up</a></li>
-    											<li><a href="#">Log In</a></li>
-    										</ul>
-    									</div>
-    								</li>
-    							</ul>
-    						</nav>
     					</header>
 
     				<!-- Main -->
@@ -307,15 +291,9 @@ def dash():
 @app.route('/search', methods=['GET', 'POST'])
 def hello():
     html ='''
-    <!DOCTYPE HTML>
-    <!--
-    	Spectral by HTML5 UP
-    	html5up.net | @n33co
-    	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-    -->
     <html>
     	<head>
-    		<title>Monsterlyrics</title>
+    		<title>MonsterLyrics</title>
     		<meta charset="utf-8" />
     		<meta name="viewport" content="width=device-width, initial-scale=1" />
                 <script src="aep000.neocities.org/assets/js/jquery.min.js"></script>
@@ -335,18 +313,19 @@ def hello():
     		<!-- Page Wrapper -->
     			<div id="page-wrapper">
 
-    				<!-- Header -->
-    					<header id="header">
-    					</header>
+				<!-- Header -->
+					<header id="header">
+						<h1><a href="index.html">MonsterLyrics</a></h1>
+					</header>
 
     				<!-- Main -->
     					<article id="main">
     						<header>
     							<h2>Search</h2>
-    							<p><form method="get" action="/search">
+    							<p><form method="get" action="search">
 										<div class="row uniform">
 											<div class="12u$">
-												<input type="text" name="search" id="demo-name" value="" placeholder="Search for an Artist, Song, or Album" />
+												<input type="text" name="search" id="search" value="" placeholder="Search for an Artist, Song, or Album" />
 
 											</div>
                                             <div class="12u$">
